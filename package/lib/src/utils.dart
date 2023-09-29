002692ed9f912ec4622268ad54453c8ba5feb0da
Feodor Fitsner <feodor@appveyor.com>
2023-09-29 16:37:37 -0700

    setupDesktop() with onClose

--- package/lib/src/utils.dart
+++ package/lib/src/utils.dart
@@ -6,10 +6,25 @@ import 'package:window_manager/window_manager.dart';
 
 import 'utils/desktop.dart';
 
-Future setupDesktop() async {
+class WindowCloseListener extends WindowListener {
+  final Function? onClose;
+
+  WindowCloseListener(this.onClose);
+
+  @override
+  void onWindowClose() {
+    onClose?.call();
+  }
+}
+
+Future setupDesktop({Function? onClose}) async {
   if (isDesktop()) {
     WidgetsFlutterBinding.ensureInitialized();
     await windowManager.ensureInitialized();
+
+    if (onClose != null) {
+      windowManager.addListener(WindowCloseListener(onClose));
+    }
 
     Map<String, String> env = Platform.environment;
     var hideWindowOnStart = env["FLET_HIDE_WINDOW_ON_START"];
